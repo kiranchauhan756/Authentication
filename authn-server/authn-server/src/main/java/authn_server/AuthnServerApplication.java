5153cb01c536f4205886f88f1a8ba4b04a8f15e7
Kiran Chauhan <kc7563330@gmail.com>
2024-10-27 13:24:52 +0530

    Add Authn Server

---
+++ authn-server/authn-server/src/main/java/authn_server/AuthnServerApplication.java
@@ -0,0 +1,13 @@
+package authn_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthnServerApplication.class, args);
+	}
+
+}
---
+++ authn-server/authn-server/src/test/java/authn_server/AuthnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package authn_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthnServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
